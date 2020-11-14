6c1bbac9a91170b4577545cab8a31ab50030a3dd
UOrnaghiProj <umberto.ornaghi@gmail.com>
2020-11-14 21:09:04 +0100

    Creato Eureka Server con 2 istanze per la MediaW app

---
+++ eureka/src/test/java/com/freeinfo/mediaw/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freeinfo.mediaw.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
